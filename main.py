0277aeeeb95d1469210bb7b9d48ccf377b2b0014
Kamil Wlodarczyk <KamilWlodarczyk2003@gmail.com>
2024-03-11 23:11:33 +0100

    create Word class

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from random import choice
+import tkinter as tk
 
-with open('text/simple.txt', 'r') as words:
-    words_list = words.readlines()
+class Word():
+    def __init__(self):
+        with open('text/simple.txt', 'r') as words:
+            self.words_list = words.readlines()
+        words_list = [word.strip() for word in self.words_list]
     
-words_list = [word.strip() for word in words_list]
-random_word = choice(words_list)
-print(random_word)
+    def pick_random(self):
+        return choice(self.words_list)
+
+    
+my_word = Word()
+
+print(my_word.pick_random())
+
+root = tk.Tk()
+root.title('Wordle')
+root.geometry("600x800")
+head_title = tk.Label(root, text="Wordle")
+head_title.grid(column=0,row=0)
+
+root.mainloop()
