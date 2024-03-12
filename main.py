71f32eec89b4b685aa767df3166e2cdc88a36060
kajtek <kajtekkajtek2003@gmail.com>
2024-03-12 21:02:46 +0100

    tworzenie klawiatury

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 BG_COLOR='#666666'
 #BLANK_SQUARE=tk.PhotoImage(file='img/Simple_blank.png')
 
+
     
 class My_App():
     def __init__(self):
@@ -21,7 +22,7 @@ class My_App():
             line_list=[]
             for x in range(0,5):
                 block_canvas = tk.Canvas(self.window, height=50, width=50, bg=BG_COLOR)
-                #block_canvas.create_image(50,50,image=self.BLANK_SQUARE)
+                block_canvas.create_image(50,50,image=self.BLANK_SQUARE)
                 line_list.append(block_canvas)
             self.squares.append(line_list)
     
@@ -30,10 +31,28 @@ class My_App():
         for y in range(0,6):
             xpos=1
             for x in range(0,5):
-                self.squares[ypos-1][xpos-1].grid(row=ypos, column=xpos, padx=2, pady=2)
+                self.squares[ypos-1][xpos-1].grid(row=ypos, column=xpos)
                 xpos+=1
             ypos+=1
-            
+
+
+
+    klawiatura="QWERTYUIOPASDFGHJKLZXCVBNM"
+    def create_keyboard(self):
+        self.keys_column=[]
+        iterator=0
+        for y in range(0,4):
+            keys_row=[]
+            if y == 1:
+                h=1
+            if y == 2:
+                h=3
+            for x in range(0,10-h):
+                keys = tk.Canvas(self.window, height=50, width=40, bg=BG_COLOR)
+                keys.create_image(50, 40, image=self.BLANK_SQUARE)
+                keys.create_text(10, 10 , self.klawiatura[iterator], font=("Arial", 12, 'bold'))
+                iterator=+1
+                self.keys_row.append(keys)
                 
         
 
